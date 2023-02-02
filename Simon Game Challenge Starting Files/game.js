87fd36ae374aa503e47fc86bbaf5420991824dd9
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-02 16:30:19 +0530

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,12 @@
-alert("hi");
+// alert("hi");
+
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomNumber]; // buttonColors[0]
+  gamePattern.push(randomChosenColour);
+}
+
